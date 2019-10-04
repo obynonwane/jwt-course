4c4048b206d28944c12902fa9b30296a3a1336ec
obinna <biostechnologyng@gmail.com>
2019-10-04 17:09:00 +0100

    Working on main

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
@@ -8,7 +9,45 @@ import (
 	"github.com/gorilla/mux"
 )
 
+//defining our model
+type User struct {
+	ID       int    `json:"id"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+type JWT struct {
+	Token string `json:"token"`
+}
+
+type Error struct {
+	Message string `json:"message"`
+}
+
+//global db object
+var db *sql.DB
+
 func main() {
+	//connection string
+	pgUrl, err := pq.ParseURL("postgres://vwbmbsno:M86A301aHWOebuEum0ypW00pTkqAOftz@salt.db.elephantsql.com:5432/vwbmbsno")
+
+	fmt.Println(db)
+
+	//check if there is error in connection
+	if err != nil {
+		log.Fatal(err)
+	}
+	//connection credentials
+	db, err = sql.Open("Postgres", pgUrl)
+
+	//check if there is error in connection
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(db)
+
+	//new router object
 	router := mux.NewRouter()
 
 	router.HandleFunc("/signup", signup).Methods("POST")
